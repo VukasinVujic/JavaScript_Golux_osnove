aac84c237761f3afcf5766fa0048d05fd281e833
VukasinVujic <vukasinvujic021@gmail.com>
2019-06-13 16:31:47 +0200

    add a new element ,with false value to show in h2

--- DOM/notes-app/notes-app.js
+++ DOM/notes-app/notes-app.js
@@ -22,13 +22,40 @@ const notes = [{
 // newElement.textContent = 'Novi text vrednost'
 // document.querySelector('body').appendChild(newElement)
 
+const filters = {
+    searchText: '',
+
+}
+
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    document.querySelector('#notes').innerHTML = '<p>TEST</P>'
+
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes,filters)
+
 document.getElementById('create-note').addEventListener('click', function(e){
     e.target.textContent = 'the button was clicked'
 })
 
-document.querySelector('#remove-all').addEventListener('click', function(e){
-    document.querySelectorAll('.note').forEach(function(note){
-        note.remove()
-    })
+document.querySelector('#search-text').addEventListener('input' , function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes,filters)
 })
 
+
+document.querySelector('#name-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target.elements.firstName.value); 
+    e.target.elements.firstName.value = ''
+
+})
--- DOM/todo-app/todos-app.js
+++ DOM/todo-app/todos-app.js
@@ -15,19 +15,83 @@ const todos = [{
     completed: true
 }]
 
-const incompleteTodos = todos.filter(function(todo){
-     return !todo.completed 
-})
-const summary = document.createElement('h2') // naprvai constantu
-summary.textContent =`You have ${incompleteTodos.length} todos left` // napuni constantu
-document.querySelector('body').appendChild(summary) // zalepi je na mesto 
 
-todos.forEach(function(todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function(todos,filters){
+    const check = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    const incompleteTodos = check.filter(function(todo){
+         return !todo.completed 
+    })
+
+    document.querySelector('#divElement').innerHTML = ''
+    
+    const summary = document.createElement('h2') // naprvai constantu
+    summary.textContent =`You have ${incompleteTodos.length} todos left` // napuni constantu
+    document.querySelector('#divElement').appendChild(summary) // zalepi je na mesto 
+    
+    check.forEach(function(element){
+        const newElement = document.createElement('p')
+        newElement.textContent = element.text   
+        document.querySelector('#divElement').appendChild(newElement)
+    })
+}
+
+renderTodos(todos,filters) // ovo mora jer kada se u filter ubaci prazno polje to svi imaju
+    // i sammim tim se svi prikazu, kada ne bi ovo stavio ne bi bio prikazan text uopste
+
+// todos.forEach(function(todo){
+//     const p = document.createElement('p')
+//     p.textContent = todo.texttodostodos
+//     document.querySelector('body').appendChild(p)
+// })
+
+// document.querySelector('#jaja').addEventListener('click',function(e){
+//         console.log('addding new to todo');     
+// })
+
+const addingNewElement = function (todos, newElementToAdd){
+    
+    document.querySelector('#divElement').innerHTML = ''
+    
+    todos.push(newElementToAdd)
+    const incompleteTodos = todos.filter(function(todo){
+        return !todo.completed 
+   })
+    
+    const summary = document.createElement('h2') // naprvai constantu
+    summary.textContent =`You have ${incompleteTodos.length} todos left` // napuni constantu
+    document.querySelector('#divElement').appendChild(summary) // zalepi je na mesto 
+    
+    todos.forEach(function(element) {
+        const oldElements = document.createElement('p')
+        oldElements.textContent = element.text
+        document.querySelector('#divElement').appendChild(oldElements)
+    })
+    console.log(todos);
+
+}
+
+// listen for input change  on div elelment
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
 
-document.querySelector('#jaja').addEventListener('click',function(e){
-        console.log('addding new to todo');     
-})
+document.querySelector('#formId').addEventListener('submit', function(e){
+    e.preventDefault()
+    let fromEvent = e.target.elements.firstName.value
+    
+    let newElementToAdd = {
+        text: fromEvent,
+        completed: false
+    }
+    e.target.elements.firstName.value = ''
+    addingNewElement(todos,newElementToAdd) 
+
+} )
