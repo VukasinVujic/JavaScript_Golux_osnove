b939ab62b3e0d342d56b5758c3d25e92fc2802fc
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-05 18:54:36 +0200

    chain of inheretance as coment

--- hangman/app.js
+++ hangman/app.js
@@ -1,8 +1,27 @@
+/*
 const product = {
     name: 'influence'
 }
 
-Object.prototype.someNewMethod = () => 'This is a new function BREEEE'
+Object.prototype.someNewMethod = () => 'This is a new function BREEEE' // a way to make a new inbuild function
 
 console.log(product.someNewMethod());
-console.log(product);
+
+*/
+// CHAIN OF INHERETANCEE 
+// Object: myObject --> Object.prototype -->null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunction --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+
+
+/*
+const team =  new Array ['Luke', 'Marko']
+console.log(team);
+console.log(team.hasOwnProperty('length'));
+*/
+
+const getScore = () => 1
+console.log(getScore);
