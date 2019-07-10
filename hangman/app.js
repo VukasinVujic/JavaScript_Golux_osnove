e114e28f7aff9402ef27ad4cc51b588a23aeeef8
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-10 11:34:36 +0200

    status(200), and message for not status 200

--- hangman/app.js
+++ hangman/app.js
@@ -53,11 +53,13 @@ window.addEventListener('keypress',(e) => { //event with presssed any key of a l
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-     if(e.target.readyState === 4){
+     if(e.target.readyState === 4 && e.target.status === 200){
          const data = JSON.parse(e.target.responseText)  
          console.log(data);
+     } else if (e.target.readyState === 4 ){
+         console.log('And error has been made BREEE');
      }
 })
 
-request.open('GET','http://puzzle.mead.io/puzzle')
+request.open('GET','http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
